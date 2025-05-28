4be8244525b0c17c88f8418642ab778c1ccaf11e
Daviraan <davi.rabelo@unesp.br>
2025-05-28 11:12:00 -0300

    primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,69 @@
+import * as SQLite from 'expo-sqlite';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import _tarefa from './types/tarefa';
+import Tarefa from './components/Tarefa';
+
+const db = SQLite.openDatabaseSync('to-do.sqlite')
 
 export default function App() {
+  const [novaTarefa, setNovaTarefa] = useState<string>('');
+  const[tarefas, setTarefas] = useState<_tarefa[]>([]);
+
+  useEffect(()=>{
+    db.execSync(`CREATE TABLE IF NOT EXISTS tarefas(
+                  id INTEGER PRIMARY KEY NOT NULL,
+                  texto VARCHAR(100),
+                  concluifo INTEGER DEFAULT 0
+                )`);
+    recarregar();
+  }, [])
+
+  const recarregar = async () => {
+    // getAllAsync - alterar tabela
+    let temp: _tarefa[] = await db.getAllAsync('SELECT * FROM tarefas');
+    setTarefas(temp);
+  }
+
+  const adicionar = async ()=>{
+    if(novaTarefa == ''){
+      Alert.alert('Insira um texto!');
+      return;
+    }
+    
+    await db.runAsync('INSERT INTO tarefas (texto) VALUES (?)', novaTarefa)
+
+    setNovaTarefa('');
+    await recarregar();
+  }
+
+  const renderLista = ()=>{
+    let lista = tarefas.map(t=>
+      <Tarefa dados={t} db={db} recarregar={recarregar} key={t.id}/>
+    );
+    return lista;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <TextInput 
+        style={styles.input}
+        value={novaTarefa} 
+        onChangeText={setNovaTarefa}
+      />
+      <Button title='Adicionar' onPress={adicionar}/>
+      <View>
+        {renderLista()}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    borderWidth: 100,
+    
   },
 });
